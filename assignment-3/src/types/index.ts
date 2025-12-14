0304259be45d98b840f0802afb98e2a9470b01e2
Pratan Nilson <119069194+Sayhighz@users.noreply.github.com>
2025-12-14 13:20:49 +0700

    feat: add type

---
+++ assignment-3/src/types/index.ts
@@ -0,0 +1,24 @@
+export type Category =
+  | 'food'
+  | 'transport'
+  | 'utilities'
+  | 'shopping'
+  | 'healthcare'
+  | 'entertainment'
+  | 'education'
+  | 'transfer'
+  | 'income'
+  | 'other';
+
+export const VALID_CATEGORIES: Category[] = [
+  'food',
+  'transport',
+  'utilities',
+  'shopping',
+  'healthcare',
+  'entertainment',
+  'education',
+  'transfer',
+  'income',
+  'other',
+];
